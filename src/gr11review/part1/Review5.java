9fa0e4864c794d626022fe15e0ce7ba2347ad4c0
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-24 23:14:09 +0000

    Create Review 3 Code

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,13 @@
 package gr11review.part1;
+import java.io.*;
+
+// javac -d bin src/gr11review/part1/Review4.java
+// java -classpath bin gr11review/part1.Review4
+public class Review5 {
+
+  public static void main(String[] args) throws Exception {
+
+
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    // Variables
