3499b8e31aeae972a24dd63166324023a168216a
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 22:48:41 +0000

    Format review 6

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -4,15 +4,23 @@ import java.text.DecimalFormat;
 
 // javac -d bin src/gr11review/part1/Review6.java
 // java -classpath bin gr11review/part1.Review6
+ 
+/**
+* A program that outputs and calculates total, tax and subtotal for items until user enters 0 as price
+@author: K. Mendes
+*
+*/
 public class Review6 {
 
   public static void main(String[] args) throws Exception {
 
+    // Adding decimal format
     DecimalFormat twoDecimalPlaces = new DecimalFormat ("0.00");
 
+    // Adding BufferedReader to get input
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-    // Variables
+    // Initialize variables
     int intItems = 1;
     int intCount = 1;
     double dblItem = 1.0;
@@ -23,9 +31,9 @@ public class Review6 {
     String strSubtotal;
     String strTax;
 
-    // While loop
-    while (dblItem != 0){
-      // Get Prices
+    // Looping until user enters 0
+    while (dblItem != 0) {
+      // Getting price of the items
       System.out.println("Enter the price for item "+(intCount)+":");
       dblItem = Double.parseDouble(keyboard.readLine());
       dblTotal = dblTotal + dblItem;
@@ -37,7 +45,7 @@ public class Review6 {
     strTax = twoDecimalPlaces.format(dblTotal * 0.13);
     strTotal = twoDecimalPlaces.format(dblTotal * 1.13);
 
-    // Output Info
+    // Output info
     System.out.println("Subtotal: $"+strSubtotal);
     System.out.println("Tax: $"+strTax);
     System.out.println("Total: $"+strTotal);
