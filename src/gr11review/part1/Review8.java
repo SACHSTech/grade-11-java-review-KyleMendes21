7929e84bb0f8cc9406ecefef27f143d6b4e969b9
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 22:54:30 +0000

    Format review 8

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -2,18 +2,24 @@ package gr11review.part1;
 import java.io.*;
 // javac -d bin src/gr11review/part1/Review8.java
 // java -classpath bin gr11review/part1.Review8
+ 
+/**
+* A program that outputs 1000 pull slots and how many triples occur
+@author: K. Mendes
+*
+*/
 public class Review8 {
 
   public static void main(String[] args) throws Exception {
 
-    // Variables
+    // Initialize variables
     int intRandom1;
     int intRandom2;
     int intRandom3;
     int intCount;
     int intTotal = 0;
 
-    // pull
+    // Creating and printing random numbers and counting how many triples
     for (intCount = 1; intCount < 1001; intCount++) {
       intRandom1 = (int)(Math.random()*5+0);
       intRandom2 = (int)(Math.random()*5+0);
@@ -24,6 +30,7 @@ public class Review8 {
       }
     }
 
+  // Printing how many triples
     System.out.println(intTotal);
 
   }
