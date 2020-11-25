d8958f12b1f9bf3cb79d51a25be8aafa51faab59
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 21:41:31 +0000

    Finish Code 7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -12,11 +12,11 @@ public class Review7 {
 
     // Variables
     int intLength;
-    int intSpaces;
-    int intA;
+    int intSpaces = 0;
+    int intA = 0;
     int intCount;
     String theSentence;
-    String strOdd;
+    String strOdd = "";
     // Char
     char chrCharacter;
 
@@ -27,7 +27,24 @@ public class Review7 {
     // something
     for (intCount = 0; intCount < intLength; intCount++) {
 
-      
+      chrCharacter = theSentence.charAt(intCount);
+
+      if (chrCharacter == ' ') {
+        intSpaces = intSpaces + 1;
+      }else if (chrCharacter == 'a' || chrCharacter == 'A') {
+        intA = intA + 1;
+      }if (intCount % 2 == 0) {
+        strOdd = strOdd + (theSentence.charAt(intCount)) + "-";
+      }
+
     }
+
+    System.out.println("There are "+intLength+ " characters in the sentence");
+    System.out.println("There are "+intSpaces+ " spaces in the sentence");
+    System.out.println("There are "+intA+" letter a's in the sentence");
+    System.out.println("Taking the odd numbered characters in the sentence produces the following string "+strOdd);
+
+  }
+}
 
     
