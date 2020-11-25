041bb36678b5a957b78f02d632a53eae8c865505
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 15:25:19 +0000

    Added code to review 7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -2,14 +2,32 @@ package gr11review.part1;
 import java.io.*;
 
 // javac -d bin src/gr11review/part1/Review7.java
-// java -classpath bin gr11review/part1.Review
+// java -classpath bin gr11review/part1.Review7
 
-public class Review5 {
+public class Review7 {
 
   public static void main(String[] args) throws Exception {
 
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
     // Variables
-    String strtheSentence;
-    int intNumber;
+    int intLength;
+    int intSpaces;
+    int intA;
+    int intCount;
+    String theSentence;
+    String strOdd;
+    // Char
+    char chrCharacter;
+
+    // input sentence and get value
+    theSentence = keyboard.readLine();
+    intLength = theSentence.length();
+
+    // something
+    for (intCount = 0; intCount < intLength; intCount++) {
+
+      
+    }
+
+    
