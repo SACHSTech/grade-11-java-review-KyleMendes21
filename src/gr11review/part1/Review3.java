31782ad7d36eee939c40c9dc7c44be565625754a
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 22:36:21 +0000

    Format review 3

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -2,22 +2,29 @@ package gr11review.part1;
 import java.io.*;
 // javac -d bin src/gr11review/part1/Review3.java
 // java -classpath bin gr11review/part1.Review3
-public class Review3{
+ 
+/**
+* A program that prints odd numbers from 20 to 100 and numbers 29 to 2 in decreasing order 
+@author: K. Mendes
+*
+*/
+public class Review3 {
 
   public static void main(String[] args) throws Exception {
     
-    // Variables
+    // Initialize variables
     int intCount;
 
-    // 20 - 100 counting
-    for (intCount = 21; intCount < 100; intCount += 2){
+    // Counting from 20 - 100 by adding 2 each time
+    for (intCount = 21; intCount < 100; intCount += 2) {
       System.out.println(intCount);
     }
 
+    // Add space to separate results
     System.out.println();
 
-    // 29 - 2
-    for (intCount = 29; intCount > 1; intCount --){
+    // Counting from 29 - 2
+    for (intCount = 29; intCount > 1; intCount --) {
       System.out.println(intCount);
     }
   
