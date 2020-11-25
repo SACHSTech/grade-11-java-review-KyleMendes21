e68ab204b09c1fe5ae690dfd3c97dd7f565bad88
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-25 22:17:31 +0000

    Format review 1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -2,41 +2,50 @@ package gr11review.part1;
 import java.io.*;
 // javac -d bin src/gr11review/part1/Review1.java
 // java -classpath bin gr11review/part1.Review1
+
+/**
+* A program that outputs the day of the year given the month and day 
+@author: K. Mendes
+*
+*/
 public class Review1 {
 
   public static void main(String[] args) throws Exception {
 
+    // Create BufferedReader
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-    // Variables
+    // Initialize Variables
     int intMonth;
     int intDay;
     int intDayNumber;
     int intCount;
     int intTotal = 0;
 
-    // Get Info
+    // Getting month and day
 		System.out.println("Enter the month number: ");
 		intMonth = Integer.parseInt(keyboard.readLine());
     System.out.println("Enter the day number: ");
 		intDay = Integer.parseInt(keyboard.readLine());
 
     // Calculate Month Total
-    for (intCount = 1; intCount < intMonth; intCount++){
-      if (intCount == 2){
+    for (intCount = 1; intCount < intMonth; intCount++) {
+      // Look at the months until the month inputted 
+      // Month 4, 6, 9, 11 have 30 days, month 2 has 28 days and month 1, 3, 5, 7, 8, 10, 12 have 31 days
+      if (intCount == 2) {
         intTotal = intTotal + 28;
       }else if (intCount == 4 || intCount == 6 || intCount == 9 || intCount == 11){
         intTotal = intTotal + 30;
-      }else{
+      }else {
         intTotal = intTotal + 31;
       }
 
     }
     
-    // Calculate day number
+    // Calculate day number using day inputted and month total from above
     intDayNumber = intTotal + intDay;
 
-    // Print Total
+    // Print result
     System.out.println(intDayNumber);
 
    }
